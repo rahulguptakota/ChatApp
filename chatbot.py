e4c0cb0cdde4715f82765a87c9ec40c9776ec168
ameyal <ameya.loya@gmail.com>
2017-10-26 03:58:50 +0530

    Retaining the frames

--- chatbot.py
+++ chatbot.py
@@ -7,6 +7,11 @@ host = '127.0.0.1'
 port = 9000
 cnt = 0
 
+var = Tk()
+
+def raise_frame(frame):
+    frame.tkraise()
+
 def Printname(event):
 	username = entry1.get()
 	password = entry2.get()
@@ -25,15 +30,27 @@ def Printname(event):
 			time.sleep(10)
 			button_1.config(state = NORMAL)
 		s.close()
+	global var
+	# var.withdraw()
+
+	choices = ['ameya' , 'shubham' , 'rahul']
+
+	f = {}
+	for i in choices :
+		f[i] = Frame(var)
+		f[i].grid(row=10, column=10, sticky='news')
+
+	Button(f[choices[0]], text='Go to frame 1', command=lambda:raise_frame(f[choices[1]])).pack()
+	Label(f[choices[0]], text='FRAME 0').pack()
+
+	Button(f[choices[1]], text='Go to frame 2', command=lambda:raise_frame(f[choices[2]])).pack()
+	Label(f[choices[1]], text='FRAME 1').pack()
+
+	Button(f[choices[2]], text='Go to frame 0', command=lambda:raise_frame(f[choices[0]])).pack()
+	Label(f[choices[2]], text='FRAME 2').pack()
+
 	s.close()
 
-# from tkinter import messagebox;
-# var = Tk()
-
-
-# print(s.recv(1024))
-# s.close()    
-var = Tk()
 name = Label(var, text = "name")
 password = Label(var, text = "password")
 entry1 = Entry(var)
@@ -46,16 +63,10 @@ c = Checkbutton(var, text = "keep me logged in")
 c.grid(row=2, columnspan=2)
 top_frame = Frame(var)
 top_frame.grid(row=3, columnspan=2)
-# bottom_frame = Frame(var)
-# bottom_frame.pack(side = BOTTOM)
-# def Printname(event):
-# 	print(entry1.get()) 
-# 	# tkSimpleDialog.askstring(entry1.get())
-# 	# answer = simpledialog.askstring(entry1.get())
-# 	messagebox.showinfo("Say Hello", "Hello World")
 button_1 = Button(top_frame, text = "Login", fg = "yellow", bg = "black")
 button_1.bind("<Button-1>", Printname)
 button_2 = Button(top_frame, text = "Reset")
 button_1.pack(side = LEFT)
 button_2.pack(side = LEFT)
+var.deiconify()
 var.mainloop()
