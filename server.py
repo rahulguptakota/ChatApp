e4f6fdcf55582642834acbc5467a2e19b187d79f
Shubham <sharma.shubham736@gmail.com>
2017-10-26 01:03:07 +0530

    select fn added

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 import socket, pickle, threading
 import os.path
 import time
+import select
 
 logged_in_users = {}
 
@@ -26,24 +27,35 @@ host = '127.0.0.1'
 port = 9000
 s.bind((host, port))
 s.listen(5)
+# Sockets from which we expect to read
+inputs = [ ]
+# Sockets to which we expect to write
+outputs = [ ]
+# Outgoing message queues (socket:Queue)
+message_queues = {}
+with open('users.txt') as f:
+		credentials = [x.strip().split(':') for x in f.readlines()]
 
 while True:
 	print("nListening for incoming connections...")
+	readable, writable, exceptional = select.select(inputs, outputs, inputs)
 	(clientsock, (ip, port)) = s.accept()
-	with open('users.txt') as f:
-		credentials = [x.strip().split(':') for x in f.readlines()]
-		data = clientsock.recv(1024)
-		[username,password] = pickle.loads(data)
-		print(username,password,credentials)
-		if [username,password] in credentials:
-			print("Authentication sucessfull")			
-			if username in logged_in_users:				
-				print(username," already logged in.")
-				clientsock.close()
-			logged_in_users[username] = [clientsock, time.time()]
-		else:
-			print("Authentication unsuccessfull")
-			clientsock.send(("Authentication Failure!!!").encode())
+	clientsock.setblocking(0)
+    inputs.append(clientsock)
+	outputs.append(clientsock)
+    message_queues[connection] = Queue.Queue()
+	data = clientsock.recv(1024)
+	[username,password] = pickle.loads(data)
+	print(username,password,credentials)
+	if [username,password] in credentials:
+		print("Authentication sucessfull")			
+		if username in logged_in_users:				
+			print(username," already logged in.")
 			clientsock.close()
+		logged_in_users[username] = [clientsock, time.time()]
+	else:
+		print("Authentication unsuccessfull")
+		clientsock.send(("Authentication Failure!!!").encode())
+		clientsock.close()
 	newthread = ClientThread(ip, port, clientsock)
 	newthread.start()
